d5071b4ea3a2e8acbffcdbd2ce525c5ddce7e363
suryansh.garg234 <suryansh.garg234@gmail.com>
2021-03-03 00:48:45 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatExpansionModule} from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { AppComponent } from './app.component';
+import { NotesService } from './notes.service';
+import { HeaderComponent } from './header/header.component';
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [ AppComponent, HeaderComponent],
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
+   ],
+  providers: [NotesService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/notes.service.ts
+++ src/app/notes.service.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Note } from './note';
 
 @Injectable()
 export class NotesService {
-
-  getNotes(): Observable<Array<Note>> {
+  constructor(private http:HttpClient){
 
   }
 
-  addNote(note: Note): Observable<Note> {
+  getNotes(): Observable<Array<Note>> {
+  return this.http.get<Array<Note>>('http://localhost:3000/notes');
+  }
 
+  addNote(note: Note): Observable<Note> {
+  return this.http.post<Note>('http://localhost:3000/notes',note);
   }
 
 }
